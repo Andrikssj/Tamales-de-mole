cf8bcdb27c5e81c15e78a51d7cdf882bf5624373
Andrikssj <l22281004@toluca.tecnm.mx>
2024-11-13 16:31:04 -0600

    Revert "Merge branch 'main' of https://github.com/Andrikssj/Tamales-de-mole"
    This reverts commit 4bd3a15b08f9071097fdc74c6924f5b81d2dba5a, reversing
    changes made to 07287b8144443105aca2517b38e07e483360536b.

--- rpg/gui/buttons/AttackButton.java
+++
@@ -1,9 +0,0 @@
-package rpg.gui.buttons;
-
-public class AttackButton extends UserButton {
-
-    public AttackButton() {
-        super("Atacar");  // Establece el texto del botón
-    }
-}
-
--- rpg/gui/ui/ImageCache.java
+++
@@ -1,9 +0,0 @@
-package rpg.gui.ui;
-
-import javax.swing.*;
-
-public class ImageCache {
-    public static ImageIcon getImageIcon(String userLeftSide) {
-        return null;
-    }
-}
